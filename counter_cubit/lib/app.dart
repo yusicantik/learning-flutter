57934b3ec79e6196d4f3e0be84c4d701685bb60c
Yushinta Prassanty <76909692+yusicantik@users.noreply.github.com>
2024-06-13 13:49:26 +0700

    Delete counter_cubit directory

--- counter_cubit/lib/app.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_counter/counter_cubit/view/counter_page.dart';
-
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key})
-      : super(
-          home: const CounterPage(),
-        );
-}
