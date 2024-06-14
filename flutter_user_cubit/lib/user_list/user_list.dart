8e2b047706f79fa1f5b2cc63a144e49dab6178ba
Yushinta Prassanty <76909692+yusicantik@users.noreply.github.com>
2024-06-14 09:56:22 +0700

    Add files via upload

---
+++ flutter_user_cubit/lib/user_list/user_list.dart
@@ -0,0 +1,5 @@
+export "package:flutter_user_cubit/user_list/user_list.dart";
+
+export "model/user.dart";
+
+export "view/user_list_page.dart";
---
+++ flutter_user_cubit/lib/user_list/view/user_list_view.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_user_cubit/user_list/cubit/user_list_cubit.dart';
+import 'package:flutter_user_cubit/user_list/cubit/user_list_state.dart';
+import 'package:flutter_user_cubit/user_list/view/user_detail_page.dart';
+
+class UserListView extends StatelessWidget {
+  const UserListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<UserListCubit, UserListState>(
+        builder: (context, state) {
+          if (state is UserListSuccess) {
+            // Success
+            return Scaffold(
+              body: ListView.builder(
+                itemCount: state.users.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      radius: 20,
+                      child: Text(
+                        state.users[index].name.substring(0, 1).toUpperCase(),
+                      ),
+                    ),
+                    title: Text(state.users[index].name),
+                    subtitle: Text(state.users[index].email),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) =>
+                              UserDetailPage(user: state.users[index]),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+              floatingActionButton: FloatingActionButton(
+                child: const Icon(Icons.refresh),
+                onPressed: () => context.read<UserListCubit>().fetchUser(),
+              ),
+            );
+          } else if (state is UserListError) {
+            // Error
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(state.errorMessage),
+                  ElevatedButton(
+                    child: const Text("Refresh"),
+                    onPressed: () => context.read<UserListCubit>().fetchUser(),
+                  ),
+                ],
+              ),
+            );
+          } else if (state is UserListLoading) {
+            // Loading
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            // Initial / First time
+            return Center(
+              child: ElevatedButton(
+                child: const Text("Refresh"),
+                onPressed: () => context.read<UserListCubit>().fetchUser(),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
